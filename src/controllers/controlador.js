0dfcde637b42ec8e13e98c807ee11d40bbb46c60
D1ALP3MU <diegoalejandroperezm@gmail.com>
2022-07-21 17:43:08 -0500

    Se cambia imagen del banner, se modifica el index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+//CREANDO VARIABLES
+
+let nombreUsuario = "Diego Pérez"
+const EDAD_USUARIO = 33
+let estaturaUsuario = 1.76
+let hinchaDelVerde = true
+
+console.log(nombreUsuario)
+
+
+console.log(EDAD_USUARIO)
