0fe5dd55e34365cdf316e37b2d5d5f34ca70ce53
407 <407@academicos.cesde.edu.co>
2022-08-16 17:38:57 -0500

    Se crea carrito

--- src/controllers/detalleProducto.js
+++ src/controllers/detalleProducto.js
@@ -4,17 +4,60 @@ console.log("Hola estoy detallando la compra")
 let producto = JSON.parse(localStorage.getItem("producto"))
 console.log(producto)
 
+let carrito
+if (JSON.parse(localStorage.getItem("carrito")) != null) {
+    carrito = JSON.parse(localStorage.getItem("carrito"))
+    let pildora = document.getElementById("pildora")
+    pildora.textContent = carrito.length
+} else {
+    carrito = []
+}
+
+let carritoMemoria = JSON.parse(localStorage.getItem("carrito"))
+console.log(carritoMemoria)
+
 //Referenciar la imagen del producto
 let foto = document.getElementById("imagenInfo")
 foto.src = producto.foto
+
 let nombre = document.getElementById("nombreInfo")
 nombre.textContent = producto.nombre
+
 let precio = document.getElementById("precioInfo")
 precio.textContent = producto.precio
+
 let descripcion = document.getElementById("descripcionInfo")
 descripcion.textContent = producto.descripcion
+
 let popularidad = document.getElementById("popularidadInfo")
 popularidad.textContent = producto.popularidad
+
+let pildora = document.getElementById("pildora")
+console.log(pildora.textContent)
+
+//escucho el click en el boton añadir al carrito
+let botonCarrito = document.getElementById("botonAgregarCarrito")
+botonCarrito.addEventListener("click", function(evento) {
+    
+    let cantidad = document.getElementById("cantidadProducto")
+    cantidad = cantidad.value
+
+    //Agregamos la cantidad al objeto producto
+    producto.cantidad = cantidad
+    console.log(producto)
+
+    //Agregamos el producto al carrito de compras
+    carrito.push(producto)
+    console.log(carrito)
+
+    //Agregando el carrito a la memoria
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+    //Pintando la pildora con la cantidad de productos
+    let cantidadCarrito = carrito.length
+    pildora.textContent = cantidadCarrito
+
+})
 
 // var item = {input1: 'input1value', input2: 'input2value'};
 // localStorage.setItem(itemIndex, JSON.stringify(item));
